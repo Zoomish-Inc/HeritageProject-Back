59a25ebd61081b2c01bf6aa5459632db98962d3e
Sitnikov Evgeniy <sitnikovevgeniy2016@yandex.ru>
2026-04-06 18:41:08 +0300

    Изменены хосты

--- config/settings.py
+++ config/settings.py
@@ -27,8 +27,17 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.getenv(
+    'DJANGO_ALLOWED_HOSTS',
+    'localhost,127.0.0.1'
+).split(',')
 
+ALLOWED_HOSTS = [host.strip() for host in ALLOWED_HOSTS]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://heritage-project-front.vercel.app',
+    'https://*.vercel.app',
+]
 
 # Application definition
 
